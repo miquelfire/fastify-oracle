bc52f741ba1eb2d950fb992afe57280df2cda0e7
Miquel "Fire" Burns <miquelfire@gmail.com>
2022-10-28 15:22:27 -0400

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+import { FastifyPluginCallback } from "fastify";
+import * as oracledb from "oracledb";
+
+export const fastifyOracle: FastifyPluginCallback<>;
+export default fastifyOracle;
+
+declare module 'fastify' {
+	interface FastifyInstance {
+		oracledb: typeof oracledb
+	}
+}
